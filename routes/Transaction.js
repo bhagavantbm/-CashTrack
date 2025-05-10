5879068d546f7d8de71c550f0b580fcf633dda39
bhagat <bhagavantmuragundi@gmail.com>
2025-05-10 23:33:36 +0530

    your message here

--- routes/Transaction.js
+++ routes/Transaction.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Transaction = require('../models/Transaction');
-const Customer = require('../models/Customer');
+const Customer = require('../models/customer');
 
 // Add a transaction for a specific customer
 // Add a transaction for a specific customer
